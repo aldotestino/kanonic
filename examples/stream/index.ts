b6f8de061b0811cce8285ab8e6ff1ce693681bcc
aldotestino <aldotestino4@gmail.com>
2026-01-17 14:00:58 +0100

    added stream example

---
+++ examples/stream/index.ts
@@ -0,0 +1,40 @@
+import { createApi, createEndpoints } from "kanonic";
+import { z } from "zod";
+
+const endpoints = createEndpoints({
+  stream: {
+    method: "GET",
+    output: z.object({
+      testing: z.boolean(),
+      sse_dev: z.string(),
+      msg: z.string(),
+      now: z.string(),
+    }),
+    path: "/test",
+    query: z.object({
+      interval: z.int().gt(0),
+    }),
+    stream: {
+      enabled: true,
+    },
+  },
+});
+
+const api = createApi({
+  baseUrl: "https://sse.dev",
+  endpoints,
+});
+
+const myStream = await api.stream({
+  query: {
+    interval: 5,
+  },
+});
+
+if (myStream.isErr()) {
+  console.error(`[${myStream.error._tag}]: ${myStream.error.message}`);
+} else {
+  for await (const chunk of myStream.value) {
+    console.log(chunk);
+  }
+}
